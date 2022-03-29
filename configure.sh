6e1febf674aaeb00b51473e9e1a5c3ff52bba79b
zlily521 <zlily988@gmail.com>
2022-03-29 17:32:42 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Download and install xRay
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/simo8102/809/releases/download/809/Xray-linux-arm64-v8a.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
+
+# Remove temporary directory
+rm -rf /tmp/xray
+
+# XRay new configuration
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
+{
+    "inbounds": [
+        {
+            "port": $PORT,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID",
+                        "level": 0
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+# Run XRay
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
